60e8b809d07049c9f10c9e6bebbdc292b3dc3579
zimmoc <128923890+zimmoc@users.noreply.github.com>
2023-11-27 15:53:58 +0100

    Add context to how the eventsList foreach function works

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,11 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(eventsList[0]);
 
         /**
-         * Lopps through the eventsList array and creates a html div for each event with sub values
+         * Loops through the eventsList array and creates a html div for each event with sub values.
+         * 
+         * The ternary operators are required to get the values because they
+         * return "undefined" without doing truthy checks on all values in
+         * the path. It just works.
          */
         eventsList.forEach((concert) =>
             htmlContent += `
@@ -81,24 +85,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
                         <p>Venue: ${concert._embedded && concert._embedded.venues && concert._embedded.venues[0] ? concert._embedded.venues[0].name : ''}</p >
 
-                        <p>${concert.classifications && concert.classifications[0] && concert.classifications[0].genre ? concert.classifications[0].genre.name : ''}</p>
+                        <p>${concert._embedded && concert._embedded.venues && concert._embedded.venues[0] ? concert._embedded.venues[0].city.name : ''}, ${concert._embedded && concert._embedded.venues && concert._embedded.venues[0] ? concert._embedded.venues[0].country.countryCode : ''}</p>
                         
-                    </div>
-                    <span class="orange-box"></span>
-                    <div class="resultTickets">
-
-
-                            <p id="shorten">Tickets go on sale: ${concert.sales && concert.sales.public ? concert.sales.public.startDateTime : ''}</p>
-                            <p>Buy tickets: <a href="${concert.url}" target="_blank">Ticketmaster</a></p>
-
                     </div>
                     <span class="orange-box"></span>
                     <div class="resultCountry">
                         <p class="dateResult">${concert.dates && concert.dates.start ? concert.dates.start.localDate : ''}</p>
 
                         <p class="venueResult"> 
-                        ${concert._embedded && concert._embedded.venues && concert._embedded.venues[0] ? concert._embedded.venues[0].city.name : ''}, ${concert._embedded && concert._embedded.venues && concert._embedded.venues[0] ? concert._embedded.venues[0].country.countryCode : ''}</p>
+                        ${concert.classifications && concert.classifications[0] && concert.classifications[0].genre ? concert.classifications[0].genre.name : ''}</p>
                     </div>
+                    <span class="orange-box"></span>
+                    <div class="resultTickets">
+
+
+                            <p id="shorten">Tickets sales: ${concert.sales && concert.sales.public ? concert.sales.public.startDateTime : ''}</p>
+                            <p>Buy tickets: <a href="${concert.url}" target="_blank">Ticketmaster</a></p>
+
+                    </div>
+                    
                 </div>
                 </div>
             </div>
